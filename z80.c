3a6a6b1db04e6b756c040dfa2d8d86b6c3f263f0
Thomas Carrill <thomas.carrill@gmail.com>
2017-02-02 12:42:42 -0500

    Started implementing interface for vm

--- z80.c
+++ z80.c
@@ -1,93 +1,139 @@
 #include "z80.h"
 
-VM vm;
+VM *init_vm() {
+	VM *vm = (VM *)malloc(sizeof(VM));
+	if (vm == NULL) {
+		return NULL;
+	}
+	vm->pc = 0;
+	vm->sp = 0;
 
-void init_vm() {
-	vm.pc = 0;
-	vm.sp = 0;
 	for (int i = 0; i < MEM_SIZE; i++) {
-		vm.memory[i] = 0;
+		vm->memory[i] = 0;
 	}
+
 	for (int i = 0; i < 8; i ++) {
-		vm.r[i] = 0;
+		vm->r[i] = 0;
 	}
+
+	return vm;
 }
 
-unsigned char fetch() {
-	if (vm.pc == MEM_SIZE) {
-		vm.pc = 0;
+void free_vm(VM *vm) {
+	free(vm);
+}
+
+unsigned char fetch(VM *vm) {
+	if (vm->pc == MEM_SIZE) {
+		vm->pc = 0;
 	}
-	return vm.memory[vm.pc++];
+	return vm->memory[vm->pc++];
 }
 
-void add(unsigned char value) {
-	vm.r[A] += value;
+void add(VM *vm, unsigned char value) {
+	vm->r[A] += value;
 }
 
-void sub(unsigned char value) {
-	vm.r[A] -= value;
+void sub(VM *vm, unsigned char value) {
+	vm->r[A] -= value;
 }
 
-void and(unsigned char value) {
-	vm.r[A] &= value;
+void and(VM *vm, unsigned char value) {
+	vm->r[A] &= value;
 }
 
-void or(unsigned char value) {
-	vm.r[A] |= value;
+void or(VM *vm, unsigned char value) {
+	vm->r[A] |= value;
 }
 
-void xor(unsigned char value) {
-	vm.r[A] ^= value;
+void xor(VM *vm, unsigned char value) {
+	vm->r[A] ^= value;
 }
 
-void load_memory(unsigned char value) {
-	unsigned short address = (fetch() << 8) | fetch();
-	vm.memory[address] = value;
+void load_register(VM *vm, unsigned short dest, unsigned short source) {
+	vm->r[dest] = vm->r[source];
 }
 
-void execute(unsigned char instr) {
+void load_memory(VM *vm, unsigned char value) {
+	unsigned short address = (fetch(vm) << 8) | fetch(vm);
+	vm->memory[address] = value;
+}
+
+void execute(VM *vm, unsigned char instr) {
 	switch(instr) {
 		case LD_A_N:
-			vm.r[A] = fetch();
+			vm->r[A] = fetch(vm);
 			break;
 		case LD_B_N: 
-			vm.r[B] = fetch(); 
+			vm->r[B] = fetch(vm); 
+			break;
+		case LD_C_N:
+			vm->r[C] = fetch(vm);
+			break;
+		case LD_D_N:
+			vm->r[D] = fetch(vm);
+			break;
+		case LD_E_N:
+			vm->r[E] = fetch(vm);
+			break;
+		case LD_H_N:
+			vm->r[H] = fetch(vm);
+			break;
+		case LD_L_N:
+			vm->r[L] = fetch(vm);
 			break;
 		case INC_B: 
-			vm.r[B] += 1; 
+			vm->r[B] += 1; 
 			break;
 		case DEC_B: 
-			vm.r[B] -= 1; 
+			vm->r[B] -= 1; 
+			break;
+		case LD_A_A:
 			break;
 		case LD_A_B: 
-			vm.r[A] = vm.r[B]; 
+			load_register(vm, A, B);
+			break;
+		case LD_A_C:
+			vm->r[A] = vm->r[C];
+			break;
+		case LD_A_D:
+			vm->r[A] = vm->r[D];
+			break;
+		case LD_A_E:
+			vm->r[A] = vm->r[E];
+			break;
+		case LD_A_H:
+			vm->r[A] = vm->r[H];
+			break;
+		case LD_A_L:
+			vm->r[A] = vm->r[L];
 			break;
 		case ADD_A_B: 
-			add(vm.r[B]); 
+			add(vm, vm->r[B]); 
 			break;
 		case SUB_B: 
-			sub(vm.r[B]); 
+			sub(vm, vm->r[B]); 
 			break;
 		case AND_B: 
-			and(vm.r[B]); 
+			and(vm, vm->r[B]); 
 			break;
 		case OR_B: 
-			or(vm.r[B]); 
+			or(vm, vm->r[B]); 
 			break;
 		case XOR_B: 
-			xor(vm.r[B]); 
+			xor(vm, vm->r[B]); 
 			break;
 		case LD_NN_A:
-			load_memory(vm.r[A]);
+			load_memory(vm, vm->r[A]);
 			break; 
 	}
 }
 
-void readBinary(char *const filename) {
+void readBinary(VM *vm, char *const filename) {
     	FILE *fp = fopen(filename, "r");
     
 	int nread;
-	unsigned char *program = vm.memory;   
+	unsigned char *program = vm->memory;   
     	do {
        	 	nread = fread(program, 1, 1, fp);
         	program++;
@@ -96,7 +142,7 @@ void readBinary(char *const filename) {
 	fclose(fp);
 }
 
-void print_registers() {
+void print_registers(VM *vm) {
 	printf("\nRegisters\n");
 	char reg = 'A';
 	for (int i = 0; i < 8; i++) {
@@ -105,28 +151,27 @@ void print_registers() {
 		} else if (i == 7) {
 			reg = 'L';
 		}
-		printf("%c: %d\t", reg, vm.r[i]);
+		printf("%c: %d\t", reg, vm->r[i]);
 		reg++;
 	}
 	printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-	init_vm();
-	readBinary(argv[1]);
-	vm.pc = 0;
+	VM *vm = init_vm();
+	readBinary(vm, argv[1]);
 	printf("Address  Opcode\n");	
 	int running = 1;
 	while(running) {
-		unsigned char instr = fetch();
-		printf("%05x    %02x\n", vm.pc - 1, instr);
+		unsigned char instr = fetch(vm);
+		printf("%05x    %02x\n", vm->pc - 1, instr);
 		if (instr == HALT) {
 			running = 0;
 		} else {				
-			execute(instr);
+			execute(vm, instr);
 		}	
 	}
-	print_registers();
-
+	print_registers(vm);
+	free_vm(vm);
 	return 0;
 }
--- z80.h
+++ z80.h
@@ -6,14 +6,78 @@
 
 #define MEM_SIZE 65536
 #define NOP 0x00
+
+//Load register operations
 #define LD_A_N 0x3E
 #define LD_B_N 0x06
+#define LD_C_N 0x0E
+#define LD_D_N 0x16
+#define LD_E_N 0x1E
+#define LD_H_N 0x26
+#define LD_L_N 0x2E
+
+#define LD_A_A 0x7F
+#define LD_A_B 0x78
+#define LD_A_C 0x79
+#define LD_A_D 0x7A
+#define LD_A_E 0x7B
+#define LD_A_H 0x7C
+#define LD_A_L 0x7D
+
+#define LD_B_A 0x47
+#define LD_B_B 0x40
+#define LD_B_C 0x41
+#define LD_B_D 0x42
+#define LD_B_E 0x43
+#define LD_B_H 0x44
+#define LD_B_L 0x45
+
+#define LD_C_A 0x4F
+#define LD_C_B 0x48
+#define LD_C_C 0x49
+#define LD_C_D 0x4A
+#define LD_C_E 0x4B
+#define LD_C_H 0x4C
+#define LD_C_L 0x4D
+
+#define LD_D_A 0x57
+#define LD_D_B 0x50
+#define LD_D_C 0x51
+#define LD_D_D 0x52
+#define LD_D_E 0x53
+#define LD_D_H 0x54
+#define LD_D_L 0x55
+
+#define LD_E_A 0x5F
+#define LD_E_B 0x58
+#define LD_E_C 0x59
+#define LD_E_D 0x5A
+#define LD_E_E 0x5B
+#define LD_E_H 0x5C
+#define LD_E_L 0x5D
+
+#define LD_H_A 0x67
+#define LD_H_B 0x60
+#define LD_H_C 0x61
+#define LD_H_D 0x62
+#define LD_H_E 0x63
+#define LD_H_H 0x64
+#define LD_H_L 0x65
+
+#define LD_L_A 0x6F
+#define LD_L_B 0x68
+#define LD_L_C 0x69
+#define LD_L_D 0x6A
+#define LD_L_E 0x6B
+#define LD_L_H 0x6C
+#define LD_L_L 0x6D
+
+#define LD_NN_A 0x32
+
 #define INC_B 0x04
 #define DEC_B 0x05
-#define LD_A_B 0x78
 #define ADD_A_B 0x80
 #define SUB_B 0x90
-#define LD_NN_A 0x32
 #define AND_B 0xA0
 #define OR_B 0xB0
 #define XOR_B 0xA8
@@ -28,7 +92,7 @@
 #define H 6
 #define L 7
 
-typedef struct {
+typedef struct VM {
 	unsigned short ix;
 	unsigned short iy;
 	unsigned short sp;
@@ -37,11 +101,10 @@ typedef struct {
 	unsigned char memory[MEM_SIZE];
 } VM; 
 
-extern VM vm;
-
-void init_vm();
-unsigned char fetch();
-void execute(unsigned char instr);
-void print_registers();
+VM *init_vm();
+unsigned char fetch(VM *vm);
+void execute(VM *vm, unsigned char instr);
+void print_registers(VM *vm);
+void free_vm(VM *vm);
 
 #endif
