d258e44715f649e9a2bcbc874ffbfda4ccf64cfb
Thomas Carrill <thomas.carrill@gmail.com>
2017-02-03 13:52:04 -0500

    Added tests for load immediate instructions

--- test.c
+++ test.c
@@ -20,6 +20,61 @@ int test(int expected, int actual) {
 	return success;
 }
 
+int testLoad() {
+	printf("testLoad()\n");
+	VM *vm = init_vm();
+	vm->memory[0] = LD_A_N;
+	vm->memory[1] = 5;
+	vm->memory[2] = HALT;
+	run(vm);
+	printf("  LD_A_N:");
+	int success = test(5, vm->r[A]);
+	
+	vm->memory[0] = LD_B_N;
+	vm->memory[1] = 5;
+	vm->memory[2] = HALT;
+	run(vm);
+	printf("  LD_B_N:");
+	success = test(5, vm->r[B]);
+
+	vm->memory[0] = LD_C_N;
+	vm->memory[1] = 5;
+	vm->memory[2] = HALT;
+	run(vm);
+	printf("  LD_C_N:");
+	success = test(5, vm->r[C]);
+
+	vm->memory[0] = LD_D_N;
+	vm->memory[1] = 5;
+	vm->memory[2] = HALT;
+	run(vm);
+	printf("  LD_D_N:");
+	success = test(5, vm->r[D]);
+
+	vm->memory[0] = LD_E_N;
+	vm->memory[1] = 5;
+	vm->memory[2] = HALT;
+	run(vm);
+	printf("  LD_E_N:");
+	success = test(5, vm->r[E]);
+
+	vm->memory[0] = LD_H_N;
+	vm->memory[1] = 5;
+	vm->memory[2] = HALT;
+	run(vm);
+	printf("  LD_H_N:");
+	success = test(5, vm->r[H]);
+
+	vm->memory[0] = LD_L_N;
+	vm->memory[1] = 5;
+	vm->memory[2] = HALT;
+	run(vm);
+	printf("  LD_L_N:");
+	success = test(5, vm->r[L]);
+
+	free_vm(vm);
+}
+
 int testAdd() {
 	printf("testAdd()\n");
 	VM *vm = init_vm();
@@ -57,6 +112,7 @@ int testSub() {
 }
 
 int main(int argc, char *argv[]) {
+	testLoad();
 	testAdd();
 	testSub();
 	return 0;		
